ad5f5517a5956c17564ba9b24b90357ca61cfb00
Adam Sheaffer <JJ06896@JJ0000M301211.local>
2017-01-16 11:56:27 -0600

    re-adding express

--- system.conf.js
+++ system.conf.js
@@ -3,27 +3,17 @@ SystemJS.config({
     meta: {
         'clock.js': {
             globals: {
-                moment: '../node_modules/moment-timezone/builds/moment-timezone-with-data.js'
+                moment: '../../node_modules/moment/moment.js'
             }
         },
         'mirror.js': {
             globals: {
-                jquery: '../node_modules/jquery/dist/jquery.js'
+                jquery: '../../node_modules/jquery/dist/jquery.js'
             }
         },
         'voiceService.js': {
             globals: {
                 annyang: '//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js'
-            }
-        },
-        'calendar.js': {
-            globals: {
-                moment: '../node_modules/moment-timezone/builds/moment-timezone-with-data.js'
-            }
-        },
-        'newsService.js': {
-            globals: {
-                jquery: '../node_modules/jquery/dist/jquery.js',
             }
         }
     },
